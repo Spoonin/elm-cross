74cc8596854f3d496314316688e2399ee94106f7
Denis Lozhkin <lozhkindenis@gmail.com>
2018-05-18 05:01:13 +0300

    game model implementation

--- models/field.ts
+++ models/field.ts
@@ -7,14 +7,74 @@ export default class Field {
     this.fieldMatrix = [];
 
     for (let i = 0; i < size; i++) {
+      // i -> row number
       this.fieldMatrix[i] = [];
 
       for (let j = 0; j < size; j++) {
+        // j column number
         this.fieldMatrix[i][j] = 0;
       }
     }
   }
-  setMove(sign: number, row: number, col: number) {
+  private getMatrixSum(): number {
+    return this.fieldMatrix
+      .reduce((acc, cur) => acc.concat(cur)) // flatten array
+      .reduce((acc, cur) => acc + cur);
+  }
+  private getRowSum(row: number): number {
+    if (row > this.size - 1) {
+      throw new Error("invalid row param");
+    }
+    return this.fieldMatrix[row].reduce((acc, cur) => acc + cur);
+  }
+  private getColSum(col: number): number {
+    if (col > this.size - 1) {
+      throw new Error("invalid col param");
+    }
+    return this.fieldMatrix.map(i => i[col]).reduce((acc, cur) => acc + cur);
+  }
+
+  private getDiagSum(): [number, number] {
+    const sumDiagonals: [number, number] = [0, 0];
+
+    for (let i = 0; i < this.size; i++) {
+      sumDiagonals[0] += this.fieldMatrix[i][i];
+      sumDiagonals[1] += this.fieldMatrix[i][this.size - i - 1];
+    }
+    return sumDiagonals;
+  }
+  getWinnerSign(): number {
+    const checkResult = (res: number) => Math.abs(res) === this.size;
+    for (let i = 0; i < this.size; i++) {
+      let result = this.getRowSum(i);
+      if (checkResult(result)) {
+        return result / Math.abs(result);
+      }
+      result = this.getColSum(i);
+      if (checkResult(result)) {
+        return result / Math.abs(result);
+      }
+    }
+    const [dm, ds] = this.getDiagSum();
+    if (checkResult(dm)) {
+      return dm / Math.abs(dm);
+    }
+    if (checkResult(ds)) {
+      return ds / Math.abs(ds);
+    }
+
+    return 0;
+  }
+  getPendingSign(): number {
+    return this.getMatrixSum() > 0 ? -1 : 1;
+  }
+  setMove(sign: number, row: number, col: number): void {
+    if (this.getPendingSign() !== sign) {
+      throw new Error("wrong turn");
+    }
+    if (this.fieldMatrix[row][col] !== 0) {
+      throw new Error("move on occupied cell");
+    }
     this.fieldMatrix[row][col] = sign;
   }
 }
--- models/game.ts
+++ models/game.ts
@@ -15,6 +15,15 @@ export default class Game {
   private getPlayer(id: string): [string, number] {
     return this.player1[0] === id ? this.player1 : this.player2;
   }
+  private getPlayerId(sign: 1 | -1): string {
+    if (sign !== 1 || sign !== <number>-1) {
+      throw new Error("invalid sign");
+    }
+    if (this.player1[1] === 0) {
+      throw new Error("sign hasn't been set");
+    }
+    return this.player1[1] === sign ? this.player1[0] : this.player2[0];
+  }
   private getOpponent(id: string): [string, number] {
     return this.player1[0] !== id ? this.player1 : this.player2;
   }
@@ -22,11 +31,24 @@ export default class Game {
     return this.player1[0] === id || this.player2[0] === id;
   }
   move(id: string, col: number, row: number): void {
+    if (this.isFinished()) {
+      throw new Error("game is ended");
+    }
     if (!this.dirty) {
       this.getPlayer(id)[1] = 1;
       this.getOpponent(id)[1] = -1;
       this.dirty = true;
     }
     this.field.setMove(this.getPlayer(id)[1], row, col);
+  }
+  getWinner(): string | null {
+    const winnerSign = this.field.getWinnerSign();
+    if (winnerSign === 1 || winnerSign === -1) {
+      return this.getPlayerId(winnerSign);
+    }
+    return null;
+  }
+  isFinished(): boolean {
+    return this.getWinner() !== null;
   }
 }
